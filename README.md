54609e7800d570474e2141903ded7e873fe7bba8
David Marin <dm@davidmarin.org>
2014-09-29 12:40:32 -0700

    add grade_to_judgment() to srs.rating

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ scrapers.
 For now, the best approach is to include this as a submodule, and link
 to the srs package::
 
-    # From the top level of your git repo
+    # From the top level of your git repo:
     mkdir submodules
     git submodule add git@github.com:spendright-scrapers/srs.git submodules/srs
     ln -s submodules/srs/srs
---
+++ srs/rating.py
@@ -0,0 +1,11 @@
+"""Utilities for deailing with ratings."""
+
+
+def grade_to_judgment(grade):
+    """Convert a letter grade (e.g. "B+") to a judgment (1 for A or B,
+    0 for C, -1 for D, E, or F).
+
+    This works for Free2Work and Rank a Brand, anyways. In theory, campaigns
+    could color their grades differently.
+    """
+    return cmp('C', grade[0].upper())
