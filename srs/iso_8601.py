c8d42a75f2ebd4233318d62f64151938a73af86b
David Marin <dm@davidmarin.org>
2014-11-21 11:30:06 -0800

    added support for ISO dates as well as datetimes

--- srs/iso_8601.py
+++ srs/iso_8601.py
@@ -1,9 +1,12 @@
 """Utilities for dealing with ISO datetime strings."""
 from __future__ import absolute_import
 
+from datetime import date
 from datetime import datetime
 
 ISO_8601_FMT = '%Y-%m-%dT%H:%M:%S.%fZ'
+
+ISO_8601_DATE_FMT = '%Y-%m-%d'
 
 
 def iso_now():
@@ -11,11 +14,26 @@ def iso_now():
     return datetime.utcnow().strftime(ISO_8601_FMT)
 
 
+def iso_today():
+    """Get the current (UTC) time as an ISO date."""
+    return datetime.utcnow().strftime(ISO_8601_DATE_FMT)
+
+
 def to_iso(utc_dt):
     """Convert a (UTC) datetime to an ISO datetime."""
     return utc_dt.strftime(ISO_8601_FMT)
 
 
+def to_iso_date(utc_date):
+    """Convert a date or datetime to an ISO date."""
+    return utc_date.strftime(ISO_8601_DATE_FMT)
+
+
 def from_iso(iso_dt):
     """Convert an ISO datetime to a (UTC) datetime object."""
     return datetime.strptime(iso_dt, ISO_8601_FMT)
+
+
+def from_iso_date(iso_date):
+    """Convert an ISO date to a date object)."""
+    return date.strptime(iso_date, ISO_8601_DATE_FMT)
